3b4a297d9713cae142e6f92d969109d120f075d1
Juan Garcia <jf.garcia268@gmail.com>
2024-03-18 09:43:27 -0400

    Test with 2 FlexCards

--- JS/reActivateFlexCards.js
+++ JS/reActivateFlexCards.js
@@ -23,7 +23,7 @@ VlocityUtils.report("Activating ALL FlexCards");
 
         const query = 'SELECT Id ' +
                       'FROM ' + package + 'VlocityCard__c ' +
-                      'WHERE ' + package + 'Active__c = true AND ' + package + "CardType__c = 'flex'";
+                      'WHERE ' + package + 'Active__c = true AND ' + package + "CardType__c = 'flex' LIMIT 2";
       
         const idsArray = await vlocity.jsForceConnection.query(query);
         //console.log(puppeteerOptions);
